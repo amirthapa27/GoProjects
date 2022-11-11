98411cef00a9fc51389f97aae3e932d18aafda1e
amirthapa27 <amirthapa687@gmail.com>
2022-11-11 16:30:51 +0530

    completed simple server

--- WebServer/main.go
+++ WebServer/main.go
@@ -1,8 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not supported", http.StatusNotAcceptable)
+		return
+	}
+	fmt.Fprintf(w, "Hello Amir")
+
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "parsing error: %v:", err)
+	}
+	fmt.Fprintf(w, "POST request successfull\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name: %s\n", name)
+	fmt.Fprintf(w, "Address: %s \n", address)
+
+}
 
 func main() {
 
-	fmt.Println("Hello World")
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Starting the server on port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
